76e1dff6b41ac0a980ec20804cafd6ddda84f84f
yusufva <yusufvalent@gmail.com>
2024-04-09 10:26:28 +0700

    fix: fixing statuscode import

--- routes/familyRoute.js
+++ routes/familyRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { statusCode } from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import familyService from "../services/familyService.js";
 import httpRespondsMessage from "../helper/httpRespondsMessage.js";
 
@@ -16,14 +16,14 @@ router.get("/", async (req, res) => {
 router.get("/:kk", async (req, res) => {
     const noKK = atob(req.params.kk);
     const family = await familyService.getByKK(noKK);
-    if (!family) return res.status(statusCode.NOT_FOUND).send(family);
+    if (!family) return res.status(StatusCodes.NOT_FOUND).send(family);
     res.send(family);
 });
 
 router.post("/", async (req, res) => {
     const family = await familyService.create(req.body);
-    if (family.statusCode == statusCode.CONFLICT)
-        return res.status(statusCode.CONFLICT).send(family);
+    if (family.statusCode == StatusCodes.CONFLICT)
+        return res.status(StatusCodes.CONFLICT).send(family);
     res.send(family);
 });
 
