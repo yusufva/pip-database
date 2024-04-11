3eb3bea963c689a1861487ef1bd30b480b2b9111
yusufva <yusufvalent@gmail.com>
2024-04-11 21:46:50 +0700

    feature: adding parsing excel middleware

--- middleware/excelParse.js
+++ middleware/excelParse.js
@@ -1,5 +1,6 @@
 import exceljs from "exceljs";
 import { StatusCodes } from "http-status-codes";
+import { unlink } from "fs";
 
 function excelHandle(req, res, next) {
     if (!req.file) {
@@ -16,22 +17,34 @@ function excelHandle(req, res, next) {
         .then((excelFile) => {
             const worksheet = excelFile.worksheets[0];
             const excelData = [];
+            const header = [];
+
+            worksheet.getRow(1).eachCell((cell, colNumber) => {
+                header[colNumber] = cell.value
+                    .replaceAll(" ", "")
+                    .toLowerCase();
+            });
 
             worksheet.eachRow((row, index) => {
                 if (index > 1) {
                     const rowData = {};
                     row.eachCell((cell, colNumber) => {
-                        rowData[`column${colNumber}`] = cell.value;
+                        rowData[`${header[colNumber]}`] = cell.value;
                     });
                     excelData.push(rowData);
                 }
             });
 
             req.excelData = excelData;
+
+            unlink(path, (err) => {
+                if (err) throw err;
+            });
             next();
         })
         .catch((err) => {
             console.error("Error reading excel file: ", err);
+            unlink(path, (err) => {});
             return res
                 .status(StatusCodes.INTERNAL_SERVER_ERROR)
                 .send(
@@ -42,4 +55,4 @@ function excelHandle(req, res, next) {
         });
 }
 
-export default excelHandle(req, res, next);
+export default excelHandle;
