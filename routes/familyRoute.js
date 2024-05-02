acb6e50b00b098a1491b1dcefb2326f07bd4e66d
yusufva <yusufvalent@gmail.com>
2024-05-02 23:26:58 +0700

    fix: fixing get by kk error and false response

--- routes/familyRoute.js
+++ routes/familyRoute.js
@@ -7,24 +7,21 @@ var router = express.Router();
 
 router.get("/", async (req, res) => {
     const families = await familyService.getAll();
-    res.send(
-        httpRespondsMessage.getSuccess("success retrieve data", families) ||
-            httpRespondsMessage.getSuccess("success retrieve data", [])
-    );
+    res.status(families.statusCode).send(families || []);
 });
 
 router.get("/:kk", async (req, res) => {
-    const noKK = atob(req.params.kk);
+    const noKK = req.params.kk;
     const family = await familyService.getByKK(noKK);
     if (!family) return res.status(StatusCodes.NOT_FOUND).send(family);
-    res.send(family);
+    res.status(family.statusCode).send(family);
 });
 
 router.post("/", async (req, res) => {
     const family = await familyService.create(req.body);
     if (family.statusCode == StatusCodes.CONFLICT)
         return res.status(StatusCodes.CONFLICT).send(family);
-    res.send(family);
+    res.status(family.statusCode).send(family);
 });
 
 export default router;
