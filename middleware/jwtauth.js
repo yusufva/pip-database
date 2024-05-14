e5141e9e3f0ae57b7784c1e8d4a1a80b63bd88b6
yusufva <yusufvalent@gmail.com>
2024-05-14 18:02:17 +0700

    fix: fixing midleware module export

--- middleware/jwtauth.js
+++ middleware/jwtauth.js
@@ -1,50 +1,75 @@
 import jwt from "jsonwebtoken";
 
-const verifyToken = async (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
-        if (err) return res.sendStatus(403);
-        req.id = decode.userId;
-        req.name = decode.name;
-        req.role = decode.role;
-        next();
-    });
-};
-
-const auth = (roles) => async (req, res, next) => {
-    try {
+function verifyToken() {
+    return async (req, res, next) => {
         const authHeader = req.headers["authorization"];
         const token = authHeader && authHeader.split(" ")[1];
-        if (token == null) return res.status(401).json({ message: "no token" });
+        if (token == null) return res.sendStatus(401);
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
             if (err) return res.sendStatus(403);
-            if (!roles.includes(decode.role))
-                return res.status(401).json({ message: "Unauthorized" });
-            req.user = decode;
+            req.id = decode.userId;
+            req.name = decode.name;
+            req.role = decode.role;
+            req.aspiratorId = decode.aspirator;
             next();
         });
-    } catch (e) {
-        res.status(500).json({ message: "something went wrong" });
-    }
-};
+    };
+}
 
-const authId = (id) => async (req, res, next) => {
-    try {
-        const authHeader = req.headers["authorization"];
-        const token = authHeader && authHeader.split(" ")[1];
-        if (token == null) return res.status(401).json({ message: "no token" });
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
-            if (err) return res.sendStatus(403);
-            if (!id.includes(decode.userId))
-                return res.status(401).json({ message: "Unauthorized" });
-            req.user = decode;
-            next();
-        });
-    } catch (e) {
-        res.status(500).json({ message: "something went wrong" });
-    }
-};
+function auth(roles) {
+    return async (req, res, next) => {
+        try {
+            const authHeader = req.headers["authorization"];
+            const token = authHeader && authHeader.split(" ")[1];
+            if (token == null)
+                return res.status(401).json({ message: "no token" });
+            jwt.verify(
+                token,
+                process.env.ACCESS_TOKEN_SECRET,
+                (err, decode) => {
+                    if (err) return res.sendStatus(403);
+                    if (!roles.includes(decode.role))
+                        return res
+                            .status(401)
+                            .json({ message: "Unauthorized" });
+                    req.user = decode;
+                    next();
+                }
+            );
+        } catch (e) {
+            res.status(500).json({ message: "something went wrong" });
+        }
+    };
+}
 
-module.exports = { auth, verifyToken, authId };
+function authId(id) {
+    return async (req, res, next) => {
+        try {
+            const authHeader = req.headers["authorization"];
+            const token = authHeader && authHeader.split(" ")[1];
+            if (token == null)
+                return res.status(401).json({ message: "no token" });
+            jwt.verify(
+                token,
+                process.env.ACCESS_TOKEN_SECRET,
+                (err, decode) => {
+                    if (err) return res.sendStatus(403);
+                    if (!id.includes(decode.userId))
+                        return res
+                            .status(401)
+                            .json({ message: "Unauthorized" });
+                    req.user = decode;
+                    next();
+                }
+            );
+        } catch (e) {
+            res.status(500).json({ message: "something went wrong" });
+        }
+    };
+}
+
+export default {
+    auth,
+    verifyToken,
+    authId,
+};
