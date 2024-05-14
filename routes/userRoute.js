1aa66dd3d0f7814e2eee1e0da672f770bc6bb7ad
yusufva <yusufvalent@gmail.com>
2024-05-14 18:04:24 +0700

    fix: fixing args

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userService from "../services/userService.js";
 
+import jwtauth from "../middleware/jwtauth.js";
+
 let router = express.Router();
 
 router.get("/", async (req, res) => {
-    const koordinators = await userService.getUserByAspirator(11);
+    const koordinators = await userService.getUserByAspirator(req.id);
     res.status(koordinators.statusCode).send(koordinators);
 });
 
