1ee51f9f18e76ca1d981badac823dc73de62a554
yusufva <yusufvalent@gmail.com>
2024-06-05 09:22:10 +0700

    feature: implement userStatistics route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,12 +5,17 @@ let router = express.Router();
 
 router.get("/", async (req, res) => {
     const koordinators = await userService.getUserByAspirator(req.id, req.role);
-    res.status(koordinators.statusCode).send(koordinators);
+    res.status(koordinators?.statusCode).send(koordinators);
 });
 
 router.get("/aspirator", async (req, res) => {
     const aspirators = await userService.getAllAspirator();
-    res.status(aspirators.statusCode).json(aspirators);
+    res.status(aspirators?.statusCode).json(aspirators);
+});
+
+router.get("/statistics", async (req, res) => {
+    const statistics = await userService.getStatistics();
+    res.status(statistics?.statusCode).json(statistics);
 });
 
 export default router;
